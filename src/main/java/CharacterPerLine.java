fb2e4abbdf07c431739337232491663ec316f015
henryapplegate <ha7applegate@gmail.com>
2025-10-19 12:42:29 -0400

    Update CharacterPerLine.java

--- src/main/java/CharacterPerLine.java
+++ src/main/java/CharacterPerLine.java
@@ -4,8 +4,8 @@ public class CharacterPerLine {
     public static void main(String[] args) {        
         // TODO: Step 1 - Get user input
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a word: ");
-        String word = sc.next();
+        System.out.print("Enter a word: ");
+        String word = sc.nextLine();
 
         // TODO: Step 2 - Print each character on a separate line
         for (int i=0; i < word.length(); i++) {
