d67cdcebe1bc2442675963cc59f1c1a04d243679
henryapplegate <ha7applegate@gmail.com>
2025-10-18 22:59:56 -0400

    Update NumberTrianglePattern.java

--- src/main/java/NumberTrianglePattern.java
+++ src/main/java/NumberTrianglePattern.java
@@ -10,7 +10,12 @@ public class NumberTrianglePattern {
 
         for(int i = 1; i <= num; i++){
             for(int j = 1; j <= i; j++){
-                System.out.print(j + " ");
+                if(j==i){
+                    System.out.print(j);
+                }
+                else{
+                    System.out.print(j+ " ");
+                }
             }
             System.out.println();
         }
